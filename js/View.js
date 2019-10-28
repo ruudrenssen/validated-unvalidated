7c3928849f5b15dd655c468c19a0fc7bb0b84ba9
ruudrenssen <ruud.renssen@gmail.com>
2019-10-28 14:28:10 +0100

    show notifications

--- js/View.js
+++ js/View.js
@@ -5,6 +5,7 @@ class View {
         this.reportingYearSelection = this.element.querySelector('[data-year-selection]');
         this.onlyUnvalidated = this.element.querySelector('[data-unvalidated-only-checkbox]');
         this.disclaimer = this.element.querySelector('[data-combined-disclaimer]');
+        this.legend = this.element.querySelector('[data-unvalidated-legend]');
 
         this.tableEl = TableFactory.createTable(this.displayData);
         this.tableUpdater = new TableUpdater(this.tableEl);
@@ -25,8 +26,16 @@ class View {
         }
     }
 
-    get combinedData () {
+    get containsCombinedData () {
         if(document.querySelector('[data-type="unvalidated"]') && document.querySelector('[data-type="validated"]')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    get containsUnvalidatedData() {
+        if(document.querySelector('[data-type="unvalidated"]')) {
             return true;
         } else {
             return false;
@@ -36,7 +45,8 @@ class View {
     updateView(e) {
         this.tableUpdater.updateBody(this.displayData);
         this.tableUpdater.updateTotals();
-        (this.combinedData)? this.disclaimer.hidden = false : this.disclaimer.hidden = true;
+        this.containsCombinedData ? this.disclaimer.hidden = false : this.disclaimer.hidden = true;
+        this.containsUnvalidatedData ? this.legend.hidden = false : this.legend.hidden = true;
     }
 
     populateYearSelection (data) {
